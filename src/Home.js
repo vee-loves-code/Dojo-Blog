453a7e45a1b124d53b1e35312765f0ac5b77614c
Dec <dec@Decagons-MacBook-Pro-7.local>
2022-09-28 23:36:32 +0100

    Props in Components

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,19 @@
 import { useState } from "react";
+import BlogList from "./BlogLists";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Vivian', id: 3 },
-        { title: 'The God\'s are not to blame ', body: 'lorem ipsum...', author: 'Ola Rotimi', id: 4 }
-      ]);
-   
-    return ( 
-        <div className="home">
-       {blogs.map((blog) => (
-        <div className="blog-preview" key = {blog.id}>
-            <h2>{ blog.title}</h2>
-            <p>Wriiten by { blog.author }</p>
-        </div>
-       ))}
-        </div>
-     );
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
+
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title = "Mario's blogs"/>
+    </div>
+  );
 }
  
 export default Home;
